f0fa4ff77786569324ece55c91647fe32e24c73d
Kuroe <rodrigolb01@gmail.com>
2025-08-15 23:17:25 -0300

    creating the database with postgres

---
+++ migrations/0000_flaky_phil_sheldon.sql
@@ -0,0 +1,16 @@
+CREATE TABLE "chats" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" text NOT NULL,
+	"message" text NOT NULL,
+	"reply" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"user_id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"email" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_name_unique" UNIQUE("name"),
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
---
+++ src/db/schema.ts
@@ -0,0 +1,22 @@
+import {pgTable, serial, text, timestamp} from 'drizzle-orm/pg-core'
+
+export const chats = pgTable('chats', {
+    id: serial('id').primaryKey(),
+    content: text('user_id').notNull(),
+    messages: text('message').notNull(),
+    reply: text('reply').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    });
+
+export const users = pgTable('users', {
+    userId: serial('user_id').primaryKey(),
+    name: text('name').notNull().unique(),
+    email: text('email').notNull().unique(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+});
+
+//Type inference for drizzle queries
+export type ChatInsert = typeof chats.$inferInsert;
+export type ChatSelect = typeof chats.$inferInsert;
+export type UserInsert = typeof users.$inferInsert;
+export type UserSelect = typeof users.$inferInsert;
